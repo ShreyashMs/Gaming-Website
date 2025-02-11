6d73d76c5bb4afa2aad83356cf77f4264e204703
ShreyashMs <shreyashmahalle010@gmail.com>
2025-02-11 22:23:50 +0530

    update feature section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -112,8 +112,81 @@ const Features = () => (
         </p>
       </div>
 
+      <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+        <BentoCard
+          src="videos/feature-1.mp4"
+          title={
+            <>
+              Charact<b>er</b>s
+            </>
+          }
+          description="Unleash the power of unique characters, each with their own abilities and style to dominate every match."
+        />
+      </BentoTilt>
 
+      <div className="grid h-[135vh] w-full grid-cols-2 grid-rows-3 gap-7">
+        <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+          <BentoCard
+            src="videos/feature-2.mp4"
+            title={
+              <>
+                Skins <b>&</b> Outfits
+              </>
+            }
+            description="Transform your look with stunning skins and outfits that combine fashion and combat-ready design."
+          />
+        </BentoTilt>
 
+        <BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+          <BentoCard
+            src="videos/feature-3.mp4"
+            title={
+              <>
+                U<b>C</b>
+              </>
+            }
+            description="Unlock exclusive gear, skins, and upgrades with UC, the ultimate currency for every BGMI champion."
+          />
+        </BentoTilt>
+
+        <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+          <BentoCard
+            src="videos/feature-4.mp4"
+            title={
+              <>
+                Compani<b>o</b>n
+              </>
+            }
+            description="Bring personality to your battles with loyal companions that add charm and flair to your gameplay."
+            isComingSoon
+          />
+        </BentoTilt>
+
+        <BentoTilt className="bento-tilt_2">
+          <div className="flex size-full flex-col justify-between  bg-[linear-gradient(45deg,_#2c3e50,_#7f8c8d,_#f39c12)]  p-5">
+            <h1 className="bento-title special-font max-w-64 text-black">
+              M<b>o</b>re co<b>m</b>ing s<b>o</b>on.
+            </h1>
+
+            <TiLocationArrow className="m-5 scale-[5] self-end" />
+          </div>
+        </BentoTilt>
+
+        <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+          <BentoCard
+            src="videos/new-event.mp4"
+            loop
+            muted
+            autoPlay
+            title={
+              <>
+              </>
+            }
+            description=""
+            isComingSoon
+          />
+        </BentoTilt>
+      </div>
     </div>
   </section>
 );
